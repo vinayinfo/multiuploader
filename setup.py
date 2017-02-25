4246c6ea390e715c98ed723c544d9d4b435cbbfb
Vinay Kumar <vk-sharma@outlook.com>
2017-02-26 01:56:52 +0530

    update setup

--- setup.py
+++ setup.py
@@ -11,12 +11,15 @@ setup(
     name='multiuploader',
     version='0.1',
     author='Vinay Kumar',
-    author_email='vk-sharma',
+    author_email='vk-sharma@outlook.com',
     packages=find_packages(),
     license='MIT License, see LICENCE.txt',
     description='Adds jQuery dynamic form for uploading multiple files',
     long_description=open('README.md').read(),
     include_package_data=True,
-    install_requires=['django>=1.4', 'sorl-thumbnail>=11.12.1b', 'python-magic'],
+    url = 'https://github.com/SharmaVinayKumar/multiuploader', # use the URL to the github repo
+    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+    keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+    install_requires=['django>=1.10', 'sorl-thumbnail>=12.3', 'python-magic'],
     zip_safe=False,
 )
