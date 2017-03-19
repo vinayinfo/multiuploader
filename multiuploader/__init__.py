6d4ac5b76038abd4ee763a3f5254affd47091a8d
Vinay Kumar <vk-sharma@outlook.com>
2017-03-19 11:29:12 +0530

    update setup

--- multiuploader/__init__.py
+++ multiuploader/__init__.py
@@ -1,2 +1,8 @@
-VERSION = (0, 2, 2)
-__version__ = '.'.join(map(str, VERSION))
+# -*- coding: utf-8 -*-
+
+"""
+Multiuploader - is an application which enable ability to upload multiple files in Django.
+https://github.com/vinaypost/multiuploader
+"""
+
+__version__ = '0.1.3'
