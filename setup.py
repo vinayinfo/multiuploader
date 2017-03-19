0a6d75927497f43ed8f34e0416a7402e9eff5050
Vinay Kumar <vk-sharma@outlook.com>
2017-03-19 11:54:43 +0530

    update setup

--- setup.py
+++ setup.py
@@ -15,15 +15,20 @@ from setuptools import find_packages
 from multiuploader import __version__
 
 CLASSIFIERS = [
-    'Development Status :: 1 - Beta',
+    'Development Status :: 4 - Beta',
     'Environment :: Web Environment',
     'Framework :: Django',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development',
